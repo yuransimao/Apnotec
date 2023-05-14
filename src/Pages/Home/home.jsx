7d503a82a13055e48b3982d6bef11bd4cb2da252
Yuran Simao <yuransimao7@gmail.com>
2023-05-14 09:36:23 +0100

    update

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,4 +1,27 @@
+import Styles from "./styles.module.scss"
+import Manlaptop from "../../image/man-laptop.png"
+
 export function Home(){
 
-    return <h1>Home</h1>
+    return (
+        <>
+        <div className={Styles.sectionCreactive}>
+
+            
+            <div className={Styles.sectionCreactive_image}>
+                <img src={Manlaptop}  alt="man-laptop"/>
+               
+        </div>
+        <div className={Styles.sectionCreactive_text}>
+            <h1>Criete web site  <br/> & Experience</h1>
+            <p>Discuss your goals, determine succes metrices identify problems to salve and define visitars of the website</p>
+            <form >
+                <input type="email" placeholder="Type your email here"/>
+                <button className={Styles.Btn}>Sign up</button>
+                
+            </form>
+        </div>
+    </div>
+    </>
+    )
 }
